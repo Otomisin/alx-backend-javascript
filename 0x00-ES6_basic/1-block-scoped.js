87ec4d18dfdad24873c66f235278b9b57e30079d
otomisin <orenaiket@yahoo.com>
2023-06-26 20:55:41 +0100

    Task 1-block-scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-var */
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    // eslint-disable-next-line
+		const task = true;
+    // eslint-disable-next-line
+		const task2 = false;
   }
 
   return [task, task2];
